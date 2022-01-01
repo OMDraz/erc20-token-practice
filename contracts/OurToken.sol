b490001ec12a2d7888bb05a36b0c59a4a3cd78bb
Omar <omardraz94@gmail.com>
2021-12-31 18:10:52 -0800

    trying to fix

--- contracts/OurToken.sol
+++ contracts/OurToken.sol
@@ -5,7 +5,36 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract OurToken is ERC20 {
+    uint256 public publicSaleStartTimestamp;
+
     constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
         _mint(msg.sender, initialSupply);
+    }
+
+    modifier whenPublicSaleActive() {
+        require(isPublicSaleOpen(), "Public sale is not open");
+        _;
+    }
+
+    modifier whenEarlyAccessActive() {
+        require(isEarlyAccessOpen(), "Early access not open");
+        _;
+    }
+
+    function isPublicSaleOpen() public view returns (bool) {
+        return
+            block.timestamp >= publicSaleStartTimestamp &&
+            publicSaleStarttimestamp != 0;
+    }
+
+    function isEarlyAccessOpen() public view returns (bool) {
+        return
+            !isPublicSaleOPen() &&
+            block.timestamp >= earlyAccessStartTimestamp &&
+            earlyAccessStartTimestamp != 0;
+    }
+
+    function setPublicSaletimestamp(uint256 timestamp) external onlyOwner {
+        publicSaleStartTimestamp = timestamp;
     }
 }
